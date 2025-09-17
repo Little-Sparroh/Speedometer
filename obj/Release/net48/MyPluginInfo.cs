9095ccae4309d0e49226a554ec2625999b9085b7
Little-Sparroh <wingedoracle@tutanota.com>
2025-09-17 12:17:04 +0000

    Add files via upload

---
+++ obj/Release/net48/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Speedometer
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Speedometer";
+        public const string PLUGIN_NAME = "Speedometer";
+        public const string PLUGIN_VERSION = "..";
+    }
+}
