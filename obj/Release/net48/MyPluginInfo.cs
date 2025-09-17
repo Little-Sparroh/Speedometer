d7575a577c9d319f3ec19be1034615d7d9572a10
Little-Sparroh <wingedoracle@tutanota.com>
2025-09-17 19:42:38 +0000

    Delete obj/Release/net48 directory

--- obj/Release/net48/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Speedometer
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "Speedometer";
-        public const string PLUGIN_NAME = "Speedometer";
-        public const string PLUGIN_VERSION = "..";
-    }
-}
